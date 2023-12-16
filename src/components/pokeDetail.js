406f09dbc385dbc612ba8ca65c8c057466826413
Fan Duc'k <81787710+Gerphan94@users.noreply.github.com>
2023-12-16 13:11:36 +0700

    1
    1

--- src/components/pokeDetail.js
+++ src/components/pokeDetail.js
@@ -5,14 +5,13 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
 
 function PokeDetail( {id, name}) {
-    const src_path = './img/' + id + '.png';
     return (
         <div className="box">
             <div className="box-img">
                 <LazyLoadImage
                 className="poke-avatar"
                 effect="blur"
-                src={ src_path} 
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} 
                 alt={name} />
             </div>
             <div>{name}</div>
