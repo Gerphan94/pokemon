7c227e6ad9e5cac63f4c57d8084bd79fb4674115
Fan Duc'k <81787710+Gerphan94@users.noreply.github.com>
2023-12-16 11:38:25 +0700

    1
    1

--- src/components/pokeDetail.js
+++ src/components/pokeDetail.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./component.css"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 
 function PokeDetail( {id, name}) {
     const src_path = './img/' + id + '.png';
     return (
         <div className="box">
             <div className="box-img">
-                <img
-                    loading="lazy"
+                <LazyLoadImage
                     className="poke-avatar" 
                     src={ src_path} 
                     alt={name}>
 
-                </img>
+                /</LazyLoadImage>
             </div>
             
             <div>{name}</div>
