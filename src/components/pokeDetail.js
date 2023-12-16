a9475c5b0883c1ac9c76b106911350ba5479bfa7
Fan Duc'k <81787710+Gerphan94@users.noreply.github.com>
2023-12-16 20:53:08 +0700

    1
    1

--- src/components/pokeDetail.js
+++ src/components/pokeDetail.js
@@ -1,21 +1,49 @@
 import React from "react";
 import "./component.css"
+import { AiOutlineClose } from "react-icons/ai";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 
-function PokeDetail( {id, name}) {
+function PokeDetail( { onClose, id, name, height, weight, types } ) {
+
+    const formattedNumber = String(id).padStart(4, '0');
+    const height_m = height/10;
+    const weight_m = weight/10;
+
     return (
-        <div className="box">
-            <div className="box-img">
-                <LazyLoadImage
-                className="poke-avatar"
-                effect="blur"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} 
-                alt={name} />
+        <div className="modal-overlay">
+        <div className="modal">
+            <div className="modal-header">
+                <div className="modal-close">
+                    <AiOutlineClose onClick={onClose}/>
+                </div>
+                
             </div>
-            <div>{name}</div>
+            <div className="modal-content">
+                <div className="content-img">
+                    <LazyLoadImage
+                        effect="blur"
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} 
+                        alt={name} />
+                </div>
+
+                <div className="content-detail">
+                    <div className="detail-row" id="poke_id">#{formattedNumber}</div>
+                    <div className="detail-row" id="poke_name">{name}</div>
+                    <div className="detail-row" id="poke_height">Height: {height_m} m</div>
+                    <div className="detail-row" id="poke_weight">Weight: {weight_m} Kg</div>
+                    <div className="detail-row">
+                    {types.map((item) => (
+                        <span className={item}>{item}</span>
+                    ))}
+                    </div>
+                    
+                </div>
+            
+            </div>
         </div>
-    )
+        </div>
+  );
 }
 export default PokeDetail;
--- src/components/pokeList.js
+++ src/components/pokeList.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokeDetail from './pokeDetail';
 import "./component.css"
 import pokeList from './pokelist.json'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { SiPokemon } from "react-icons/si";
 
 function PokeList() {
+
+    const [detailID, setDetailID] = useState('');
+    const [detailName, setDetailName] = useState('');
+    const [detailW, setDetailW] = useState('');
+    const [detailH, setDetailH] = useState('');
+    const [detailType, setDetailType] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = (id, name, height, weight, types) => {
+        setShowModal(true);
+        setDetailID(id);
+        setDetailName(name);
+        setDetailH(height)
+        setDetailW(weight);
+        setDetailType(types);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
+
     return (
-        <div className='container'>
-            <div className='title'>1000 Pokémons</div>
+        <div>
+            <div className='container'>
+                <SiPokemon className='title-icon'/> 
+                
                 <div className='grid'>
                     {pokeList.map((pokemon) => (
-                        <PokeDetail key={pokemon.id} id={pokemon.id} name={pokemon.name}/> 
+                        <div key={pokemon.id} 
+                            className="box" 
+                            onClick={() => openModal(pokemon.id, pokemon.name, pokemon.height, pokemon.weight, pokemon.types)} >
+                            <div className="box-img">
+                                <LazyLoadImage
+                                className="poke-avatar"
+                                effect="blur"
+                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} 
+                                alt={pokemon.name} />
+                            </div>
+                            <div>{pokemon.name}</div>
+                        </div>
                     ))}
                 </div>
+            </div>
+            {showModal && 
+                <PokeDetail 
+                    onClose={closeModal} 
+                    id={detailID} 
+                    name={detailName} 
+                    height={detailH} 
+                    weight={detailW}
+                    types={detailType}
+                    />
+            }
         </div>
     )
 }
